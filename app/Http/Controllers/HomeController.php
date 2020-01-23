0cc0ed67d20a6bb1ec00519189a8bde8193b02ff
Josh Larson <jplhomer@gmail.com>
2020-01-22 22:18:31 -0600

    Send along user info

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', ['user' => Auth::user()->loadMissing('currentTeam', 'teams')]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,13 +13,11 @@
             <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
 
                 <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
-                    Dashboard
+                    {{ $user->currentTeam->name }} Dashboard
                 </div>
 
                 <div class="w-full p-6">
-                    <p class="text-gray-700">
-                        You are logged in!
-                    </p>
+
                 </div>
             </div>
         </div>
