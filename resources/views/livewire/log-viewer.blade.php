9de2d84019e35147c1071fed9e92e2789e86698f
Josh Larson <jplhomer@gmail.com>
2020-04-14 20:06:04 -0500

    Disable inputs when polling is happening

--- resources/views/livewire/log-viewer.blade.php
+++ resources/views/livewire/log-viewer.blade.php
@@ -1,12 +1,12 @@
 <div wire:poll>
     <div class="flex md:justify-between text-sm mb-2 text-gray-600">
         <div>
-            <label class="mr-1"><input type="radio" name="service" value="web" wire:model="service"> Web</label>
-            <label><input type="radio" name="service" value="worker" wire:model="service"> Worker</label>
+            <label class="mr-1"><input type="radio" name="service" value="web" wire:model="service" wire:loading.attr="disabled"> Web</label>
+            <label><input type="radio" name="service" value="worker" wire:model="service" wire:loading.attr="disabled"> Worker</label>
         </div>
         <div>
-            <label class="mr-1"><input type="radio" name="logType" value="all" wire:model="logType"> All logs</label>
-            <label><input type="radio" name="logType" value="app" wire:model="logType"> App logs only</label>
+            <label class="mr-1"><input type="radio" name="logType" value="all" wire:model="logType" wire:loading.attr="disabled"> All logs</label>
+            <label><input type="radio" name="logType" value="app" wire:model="logType" wire:loading.attr="disabled"> App logs only</label>
         </div>
     </div>
     <div class="font-mono p-2 bg-gray-300 text-xs overflow-y-scroll h-64 w-full block">
