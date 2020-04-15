3f9bf66c57b52aa3f69cc332900c9e4657d45438
Josh Larson <jplhomer@gmail.com>
2020-04-14 22:04:29 -0500

    Switch to 5s

--- resources/views/livewire/log-viewer.blade.php
+++ resources/views/livewire/log-viewer.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll>
+<div wire:poll.5s>
     <div class="flex md:justify-between text-sm mb-2 text-gray-600">
         <div>
             <label class="mr-1"><input type="radio" name="service" value="web" wire:model="service" wire:loading.attr="disabled"> Web</label>
