433f23882c7f1599868692e2be4bee529e49b8dd
Josh Larson <jplhomer@gmail.com>
2020-05-05 11:18:25 -0500

    Allow a deployment to be deployed from latest hash

--- resources/views/livewire/deployments-list.blade.php
+++ resources/views/livewire/deployments-list.blade.php
@@ -1,4 +1,8 @@
 <div>
+    <div class="flex justify-between items-center">
+        <x-subtitle>Recent Deployments</x-subtitle>
+        <x-white-button wire:click="deployNow">Deploy Now</x-white-button>
+    </div>
     <div class="flex flex-col">
         <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
             <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
