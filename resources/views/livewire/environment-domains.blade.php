253ea0d8f97eced55d3dda1c6d9c72f57b699bb9
Josh Larson <jplhomer@gmail.com>
2020-05-11 21:58:11 -0500

    Move domains to a table

--- resources/views/livewire/environment-domains.blade.php
+++ resources/views/livewire/environment-domains.blade.php
@@ -1,10 +1,26 @@
 <div wire:poll>
     @if (count($mappings) > 0)
-        <ul class="mb-4">
-            @foreach ($mappings as $mapping)
-                <li>{{ $mapping->domain }} - <x-status :status="$mapping->status" /><br>{!! $mapping->message !!}</li>
+        <x-table>
+            <x-slot name="thead">
+                <x-th>Domain</x-th>
+                <x-th>Status</x-th>
+                <x-th>Instructions</x-th>
+                <x-th last />
+            </x-slot>
+
+            @foreach ($mappings as $idx => $mapping)
+                <x-tr :idx="$idx">
+                    <x-td>{{ $mapping->domain }}</x-td>
+                    <x-td><x-status :status="$mapping->status" /></x-td>
+                    <x-td>
+                        <div class="text-xs">
+                            {!! $mapping->message !!}
+                        </div>
+                    </x-td>
+                    <x-td last />
+                </x-tr>
             @endforeach
-        </ul>
+        </x-table>
     @endif
 
     <h3 class="text-lg mb-2">Assign a Custom Domain</h3>
