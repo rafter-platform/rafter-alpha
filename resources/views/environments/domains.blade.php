8970a2c4dc97f5aa5e5147023f0c02855d8fbab9
Josh Larson <jplhomer@gmail.com>
2020-05-14 14:27:55 -0500

    Add some helperoos

--- resources/views/environments/domains.blade.php
+++ resources/views/environments/domains.blade.php
@@ -2,7 +2,19 @@
     <x-environment :project="$project" :environment="$environment">
         <x-slot name="title">Domains</x-slot>
 
-        <p class="mb-4 text-gray-600">Assign one or more vanity domains to your service.</p>
+        @if ($environment->url)
+            <x-description>
+                By default, Cloud Run has assigned your service a URL of:
+            </x-description>
+
+            <ul class="mb-4 text-sm">
+                <li><a class="underline text-blue-600" href="{{ $environment->url }}"><code>{{ $environment->url }}</code></a></li>
+            </ul>
+        @endif
+
+        <x-description>
+            You can assign one or more custom domains to your service. Add your custom domain name below, and follow the instructions.
+        </x-description>
 
         <livewire:environment-domains :environment="$environment" />
     </x-environment>
