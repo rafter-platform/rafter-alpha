99311c6de8ba76485eaa8fbb54e1875fa3add180
Josh Larson <jplhomer@gmail.com>
2020-05-15 11:20:45 -0500

    Add DomainMappingPolicy

--- app/Http/Controllers/EnvironmentDomainsController.php
+++ app/Http/Controllers/EnvironmentDomainsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\DomainMapping;
 use App\Environment;
 use App\Project;
 use Illuminate\Http\Request;
@@ -10,6 +11,8 @@ class EnvironmentDomainsController extends Controller
 {
     public function __invoke(Project $project, Environment $environment)
     {
+        $this->authorize('viewAny', [DomainMapping::class, $environment]);
+
         return view('environments.domains', [
             'project' => $project,
             'environment' => $environment,
---
+++ app/Policies/DomainMappingPolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Policies;
+
+use App\DomainMapping;
+use App\Environment;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class DomainMappingPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user, Environment $environment)
+    {
+        return $user->currentTeam->is($environment->project->team);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\DomainMapping  $domainMapping
+     * @return mixed
+     */
+    public function view(User $user, DomainMapping $domainMapping)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\DomainMapping  $domainMapping
+     * @return mixed
+     */
+    public function update(User $user, DomainMapping $domainMapping)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\DomainMapping  $domainMapping
+     * @return mixed
+     */
+    public function delete(User $user, DomainMapping $domainMapping)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\DomainMapping  $domainMapping
+     * @return mixed
+     */
+    public function restore(User $user, DomainMapping $domainMapping)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\DomainMapping  $domainMapping
+     * @return mixed
+     */
+    public function forceDelete(User $user, DomainMapping $domainMapping)
+    {
+        //
+    }
+}
