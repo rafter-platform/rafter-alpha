f83d81e541a713d3f855007fc498c67e55407d7b
Lucas Michot <lucas.michot@shopify.com>
2020-05-25 20:27:55 +0200

    Comply PSR-4 standards

--- tests/Feature/EnvironmentDomainsControllerTest.php
+++ tests/Feature/EnvironmentDomainsControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class EnvironmentDomainsController extends TestCase
+class EnvironmentDomainsControllerTest extends TestCase
 {
     use RefreshDatabase;
 
