f9774e6e43b99de1fe23ce663a9b20d1c6a436f8
Lucas Michot <lucas.michot@shopify.com>
2020-05-26 20:10:55 +0200

    Use direct ::class notation without namespace prefixing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
+use App\Http\Controllers\BuildInstructionsController;
+use App\Http\Controllers\CommandController;
+use App\Http\Controllers\DatabaseInstanceController;
+use App\Http\Controllers\DeploymentController;
+use App\Http\Controllers\EnvironmentController;
+use App\Http\Controllers\EnvironmentDatabaseController;
+use App\Http\Controllers\EnvironmentDomainsController;
+use App\Http\Controllers\EnvironmentLogController;
+use App\Http\Controllers\EnvironmentSettingsController;
+use App\Http\Controllers\GoogleProjectController;
+use App\Http\Controllers\HookDeploymentController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\RedeployDeploymentController;
+use App\Http\Controllers\SourceProviderController;
 use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,68 +33,66 @@ if (app()->environment('local')) {
 }
 
 // Dynamic Dockerfiles and entrypoints for Cloud Build
-Route::get('/build/{type}/{file}', 'BuildInstructionsController@show')->name('build-instructions');
+Route::get('/build/{type}/{file}', BuildInstructionsController::class.'@show')->name('build-instructions');
 
 // Incoming GitHub webhooks
-Route::post('/hooks/{type}', 'HookDeploymentController@store');
+Route::post('/hooks/{type}', HookDeploymentController::class.'@store');
 
 // TODO: Remove
 Route::get('/test', function () {
     TestJob::dispatch(12);
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
     // Dashboard
-    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/home', HomeController::class.'@index')->name('home');
 
     // Google Projects
-    Route::resource('google-projects', 'GoogleProjectController');
+    Route::resource('google-projects', GoogleProjectController::class);
 
     // Projects
-    Route::resource('projects', 'ProjectController');
+    Route::resource('projects', ProjectController::class);
 
     // Environments
-    Route::resource('projects.environments', 'EnvironmentController');
+    Route::resource('projects.environments', EnvironmentController::class);
 
     // Environment Databases
-    Route::resource('projects.environments.database', 'EnvironmentDatabaseController')
+    Route::resource('projects.environments.database', EnvironmentDatabaseController::class)
         ->only(['index', 'store', 'destroy']);
 
     // Environment Settings
-    Route::resource('projects.environments.settings', 'EnvironmentSettingsController')
+    Route::resource('projects.environments.settings', EnvironmentSettingsController::class)
         ->only(['index', 'store']);
 
     // Deployments
-    Route::resource('projects.environments.deployments', 'DeploymentController');
-    Route::post('projects/{project}/environments/{environment}/deployments/{deployment}/redeploy', 'RedeployDeploymentController')
+    Route::resource('projects.environments.deployments', DeploymentController::class);
+    Route::post('projects/{project}/environments/{environment}/deployments/{deployment}/redeploy', RedeployDeploymentController::class)
         ->name('projects.environments.deployments.redeploy');
 
     // Logs
-    Route::get('projects/{project}/environments/{environment}/logs', 'EnvironmentLogController')
+    Route::get('projects/{project}/environments/{environment}/logs', EnvironmentLogController::class)
         ->name('projects.environments.logs');
 
     // Domains
-    Route::get('projects/{project}/environments/{environment}/domains', 'EnvironmentDomainsController')
+    Route::get('projects/{project}/environments/{environment}/domains', EnvironmentDomainsController::class)
         ->name('projects.environments.domains');
 
     // Commands
-    Route::get('projects/{project}/environments/{environment}/commands', 'CommandController@index')
+    Route::get('projects/{project}/environments/{environment}/commands', CommandController::class.'@index')
         ->name('projects.environments.commands.index');
-    Route::get('projects/{project}/environments/{environment}/commands/{command}', 'CommandController@show')
+    Route::get('projects/{project}/environments/{environment}/commands/{command}', CommandController::class.'@show')
         ->name('projects.environments.commands.show');
 
     // Database Instances
-    Route::resource('database-instances', 'DatabaseInstanceController');
+    Route::resource('database-instances', DatabaseInstanceController::class);
 
     // Source Providers
-    Route::resource('source-providers', 'SourceProviderController');
+    Route::resource('source-providers', SourceProviderController::class);
 
     // Inbound Source Provider Authorizations
-    Route::get('auth/github', 'SourceProviderController@store');
+    Route::get('auth/github', SourceProviderController::class.'@store');
 });
