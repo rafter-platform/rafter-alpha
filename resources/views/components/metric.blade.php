0eee18363661a39831bff8549f122aaca2c9a90f
Josh Larson <jplhomer@gmail.com>
2020-06-12 06:04:09 -0500

    Add Metric component

---
+++ resources/views/components/metric.blade.php
@@ -0,0 +1,12 @@
+<div class="bg-white overflow-hidden shadow rounded-lg">
+    <div class="px-4 py-5 sm:p-6">
+        <dl>
+            <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
+                {{ $title }}
+            </dt>
+            <dd class="mt-1 text-3xl leading-9 font-semibold text-gray-900">
+                {{ $slot }}
+            </dd>
+        </dl>
+    </div>
+</div>
--- resources/views/livewire/environment-metrics.blade.php
+++ resources/views/livewire/environment-metrics.blade.php
@@ -3,41 +3,8 @@
         Last 24 hours
     </h3>
     <div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <div class="bg-white overflow-hidden shadow rounded-lg">
-            <div class="px-4 py-5 sm:p-6">
-                <dl>
-                    <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
-                        Total Subscribers
-                    </dt>
-                    <dd class="mt-1 text-3xl leading-9 font-semibold text-gray-900">
-                        71,897
-                    </dd>
-                </dl>
-            </div>
-        </div>
-        <div class="bg-white overflow-hidden shadow rounded-lg">
-            <div class="px-4 py-5 sm:p-6">
-                <dl>
-                    <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
-                        Avg. Open Rate
-                    </dt>
-                    <dd class="mt-1 text-3xl leading-9 font-semibold text-gray-900">
-                        58.16%
-                    </dd>
-                </dl>
-            </div>
-        </div>
-        <div class="bg-white overflow-hidden shadow rounded-lg">
-            <div class="px-4 py-5 sm:p-6">
-                <dl>
-                    <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
-                        Avg. Click Rate
-                    </dt>
-                    <dd class="mt-1 text-3xl leading-9 font-semibold text-gray-900">
-                        24.57%
-                    </dd>
-                </dl>
-            </div>
-        </div>
+        <x-metric title="Total Requests">12,355</x-metric>
+        <x-metric title="Web Requests">8,355</x-metric>
+        <x-metric title="Worker Requests">4,000</x-metric>
     </div>
 </div>
