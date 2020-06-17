27712debf48778cb78f77bbce17f2edfc39cfed5
Josh Larson <jplhomer@gmail.com>
2020-06-17 13:15:36 -0500

    Allow an existing Cloud Run service to exist

--- app/Jobs/CreateCloudRunService.php
+++ app/Jobs/CreateCloudRunService.php
@@ -2,9 +2,11 @@
 
 namespace App\Jobs;
 
+use App\GoogleCloud\CloudRunConfig;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
@@ -14,8 +16,26 @@ class CreateCloudRunService implements ShouldQueue
 
     public function handle()
     {
-        $this->model->createCloudRunService();
-        $this->model->createCloudRunWorkerService();
+        try {
+            $this->model->createCloudRunService();
+            $this->model->createCloudRunWorkerService();
+        } catch (RequestException $e) {
+            $body = $e->response->json();
+
+            if ($body['error']['status'] == 'ALREADY_EXISTS') {
+                $config = new CloudRunConfig($this->model);
+
+                $this->model->environment->setWebName($config->name());
+                $this->model->environment->setWorkerName($config->forWorker()->name());
+
+                $this->model->updateCloudRunService();
+                $this->model->updateCloudRunWorkerService();
+
+                return "Existing Cloud Run services detected. Updating those services with the latest image instead.";
+            }
+
+            throw $e;
+        }
 
         return true;
     }
