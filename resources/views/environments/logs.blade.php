48dd135a6c41406b081aead02ba3ce5738213370
Josh Larson <jplhomer@gmail.com>
2020-06-26 19:52:32 -0500

    Add helper text

--- resources/views/environments/logs.blade.php
+++ resources/views/environments/logs.blade.php
@@ -2,7 +2,10 @@
     <x-environment :project="$project" :environment="$environment">
         <x-slot name="title">Logs</x-slot>
 
-        <p class="mb-4 text-gray-600">View the logs from your service.</p>
+        <p class="mb-4 text-gray-600">
+            View the logs from your service, or view all logs by visiting your
+            <a class="text-blue-600 underline" href="https://console.cloud.google.com/run?project={{ $environment->projectId() }}" target="_blank" rel="noopener">Cloud Run dashboard</a>.
+        </p>
 
         <livewire:log-viewer :environment="$environment" />
     </x-environment>
